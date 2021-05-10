20a993a825790edaedfb89042f65d7f598641ee5
khegde <kshegde6@gmail.com>
2021-05-09 19:49:07 -0700

    temp sh files for baselines

---
+++ multi_sound_tester_cute.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Cute multi sounds
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoom_1 --encoder_custom vizdoom
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoom_2 --encoder_custom vizdoom
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoom_3 --encoder_custom vizdoom
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundSamples_1 --encoder_custom vizdoomSoundSamples
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundSamples_2 --encoder_custom vizdoomSoundSamples
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundSamples_3 --encoder_custom vizdoomSoundSamples
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundLogMel_1 --encoder_custom vizdoomSoundLogMel
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundLogMel_2 --encoder_custom vizdoomSoundLogMel
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundLogMel_3 --encoder_custom vizdoomSoundLogMel
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundFFT_1 --encoder_custom vizdoomSoundFFT
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundFFT_2 --encoder_custom vizdoomSoundFFT
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_sound_multi --experiment multisound_cute_vizdoomSoundFFT_3 --encoder_custom vizdoomSoundFFT
---
+++ multi_sound_tester_hell.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# hell multi sounds
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoom_1 --encoder_custom vizdoom
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoom_2 --encoder_custom vizdoom
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoom_3 --encoder_custom vizdoom
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundSamples_1 --encoder_custom vizdoomSoundSamples
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundSamples_2 --encoder_custom vizdoomSoundSamples
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundSamples_3 --encoder_custom vizdoomSoundSamples
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundLogMel_1 --encoder_custom vizdoomSoundLogMel
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundLogMel_2 --encoder_custom vizdoomSoundLogMel
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundLogMel_3 --encoder_custom vizdoomSoundLogMel
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundFFT_1 --encoder_custom vizdoomSoundFFT
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundFFT_2 --encoder_custom vizdoomSoundFFT
+
+python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 24 --num_envs_per_worker 20 --env doom_hell_sound_multi --experiment multisound_hell_vizdoomSoundFFT_3 --encoder_custom vizdoomSoundFFT
